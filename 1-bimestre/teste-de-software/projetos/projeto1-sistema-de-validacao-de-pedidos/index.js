0d4da5a44d6f15dc1e96d00f3c6de4c949a84b8c
Evelin <evelinmariabarroso@gmail.com>
2026-02-28 17:33:27 +0000

    ok

---
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/index.js
@@ -0,0 +1,16 @@
+function validarPedido(pedido) {
+  if (!pedido.cliente) {
+    return false
+  }
+
+  if (pedido.total <= 0) {
+    return false
+  }
+
+  if (!pedido.itens || pedido.itens.length === 0) {
+    return false
+  }
+
+  return true
+}
+module.exports = validarPedido
